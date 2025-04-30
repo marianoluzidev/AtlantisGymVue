6a665d502572ef70c1bf3dbbb8041f7599866b2d
Mariano Luzi <marianoluzi@hotmail.com>
2025-04-30 14:23:41 -0300

    segundo commit

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,69 +1,89 @@
 <template>
-  <f7-page name="home">
+  <f7-page name="home" class="home" >
     <!-- Top Navbar -->
-    <f7-navbar>
-      <f7-nav-title class="text-yellow-400">Atlantis Gym</f7-nav-title>
-    </f7-navbar>
+    <f7-navbar title="ATLANTIS GYM" />
 
+    <!-- Motivational Image + Quote -->
+    <div class="motivational-banner">
+      <img src="../img/mancuernas.png" alt="Dumbbell" />
+      <div class="quote">NO PAIN<br />NO GAIN</div>
+    </div>
 
-    <!-- Page content-->
-    <f7-block>      
-      
-      <div class="text-5xl font-bold text-yellow-400">
-        Bienvenido, Mariano
+    <f7-block >
+      <div class="bienvenida-cliente">
+        <h2>BIENVENIDO, MARCELO</h2>
+        <div class="info-cliente">
+          <div class="info-box">
+            <div class="info-label">Peso</div>
+            <div class="info-value">{{ cliente.peso }} kg</div>
+          </div>
+          <div class="info-box">
+            <div class="info-label">Altura</div>
+            <div class="info-value">{{ cliente.altura }} cm</div>
+          </div>
+          <div class="info-box">
+            <div class="info-label">Objetivo</div>
+            <div class="info-value">{{ cliente.objetivo }}</div>
+          </div>
+        </div>
+      </div>  
+    </f7-block>    
+
+    <!-- Cuota Section -->
+    
+    <f7-block>
+
+      <div class="estado-cuota">
+      <div class="cuota-card">
+        <div class="cuota-estado">CUOTA VENCIDA</div>
+        <button class="btn-pagar" >PAGAR</button>
+      </div>
+    </div>
+    
+    </f7-block>
+
+    <!---
+    <f7-block class="rutina-section" @click="$f7.views.main.router.navigate('/rutina/')">
+      <div class="rutina-fixed">
+        <f7-block class="rutina-section" @click="$f7.views.main.router.navigate('/rutina/')">
+          <div class="rutina-content">
+            <div>
+              <div class="rutina-title">RUTINA</div>
+              <div class="rutina-link">Ver rutina actual</div>
+            </div>
+            <div class="arrow">›</div>
+          </div>
+        </f7-block>
       </div>
     </f7-block>
-
-    <f7-block-title>Navigation</f7-block-title>
-    <f7-list class="fondo-amarillo"  inset dividersIos>
-      <f7-list-item link="/about/" title="About"></f7-list-item>
-      <f7-list-item link="/form/" title="Form"></f7-list-item>
-    </f7-list>
-
-    <f7-block-title>Modals</f7-block-title>
-    <f7-block class="grid grid-cols-2 grid-gap">
-      <f7-button fill class="btn-negro-sobre-amarillo" popup-open="#my-popup">Popup</f7-button>
-      <f7-button fill class="btn-negro-sobre-amarillo" login-screen-open="#my-login-screen">Login Screen</f7-button>
-    </f7-block>
-
-    <f7-block-title>Panels</f7-block-title>
-    <f7-block class="grid grid-cols-2 grid-gap">
-      <f7-button fill class="btn-negro-sobre-amarillo" panel-open="left">Left Panel</f7-button>
-      <f7-button fill class="btn-negro-sobre-amarillo" panel-open="right">Right Panel</f7-button>
-    </f7-block>
-
+    -->
+    
+  <f7-block-title>Rutinas</f7-block-title>
     <f7-list strong inset dividersIos>
-      <f7-list-item
-        title="Dynamic (Component) Route"
-        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
-      ></f7-list-item>
-      <f7-list-item
-        title="Default Route (404)"
-        link="/load-something-that-doesnt-exist/"
-      ></f7-list-item>
-      <f7-list-item
-        title="Request Data & Load"
-        link="/request-and-load/user/123456/"
-      ></f7-list-item>
+      <f7-list-item link="/about/" title="Rutina actual"></f7-list-item>
+      <f7-list-item link="/about/" title="Pagos"></f7-list-item>
+      <f7-list-item link="/about/" title="Mi Perfil"></f7-list-item>
     </f7-list>
+
   </f7-page>
 </template>
 
 <script>
-import { db } from '../js/firebase';
-import { getFirestore,collection, getDocs } from "firebase/firestore";
-
-
 export default {
-  async mounted() {
-    
-    const clientesCol = collection(db, "usuario");
-    const clientesSnapshot = await getDocs(clientesCol);
-    const clientesList = clientesSnapshot.docs.map(doc => ({
-      id: doc.id,
-      ...doc.data()
-    }));
-    console.log(clientesList);
+  data() {
+    return {
+      cliente: {
+        nombre: 'Mariano',
+        peso: 82,
+        altura: 178,
+        objetivo: 'Musculación',
+        cuota: 'Cuota vencida' // o 'Al día'
+      }
+    };
   }
-}
+};
 </script>
+
+<style scoped>
+
+</style>
